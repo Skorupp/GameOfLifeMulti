578837771fc5411341f69fbd0328a455513d6582
Jakub Skorupa <diegoskorup@gmail.com>
2023-12-29 17:01:33 +0100

    Added neighbour validation and hardcoded new iteration

--- src/CellManager.java
+++ src/CellManager.java
@@ -1,18 +1,16 @@
 import java.util.concurrent.*;
 
-public class CellManager implements Runnable{
+public class CellManager implements Runnable {
 
     private char[][] map;
-    private char[][] newMap;
     private int rows;
     private int columns;
     private int simLength;
 
-    public CellManager (char [][] map, int simLength){
+    public CellManager(char[][] map, int simLength) {
         this.map = map;
         this.rows = map.length;
         this.columns = map[0].length;
-        this.newMap = map;
         this.simLength = simLength;
     }
 
@@ -22,4 +20,76 @@ public class CellManager implements Runnable{
 
     }
 
+    public Integer CountNeighbours(char[][] map, int row, int col) {
+        int count = 0;
+        int x_toCheck;
+        int y_toCheck;
+        boolean xSet = false;
+        boolean ySet = false;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                x_toCheck = 0;
+                y_toCheck = 0;
+                if (i == 0 && j == 0) continue;
+                if (row + i < 0) {
+                    x_toCheck = map.length - 1;
+                    xSet = true;
+                }
+                if (row + i >= map.length) {
+                    x_toCheck = 0;
+                    xSet = true;
+                }
+                if (col + j < 0) {
+                    y_toCheck = map[0].length - 1;
+                    ySet = true;
+                }
+                if (col + j >= map[0].length) {
+                    y_toCheck = 0;
+                    ySet = true;
+                }
+                if (!xSet) {
+                    x_toCheck = row + i;
+                }
+                if (!ySet) {
+                    y_toCheck = col + j;
+                }
+                ySet = false;
+                xSet = false;
+                if (map[x_toCheck][y_toCheck] == '1') count++;
+            }
+        }
+        return count;
+    }
+
+    public char[][] newIteration (char[][] map){
+        char [][] newMap = map;
+        for (int i = 0; i <= map.length - 1; i++){
+            for(int j = 0; j <= map[0].length - 1; j++){
+                int neighbours = CountNeighbours(map, i , j);
+
+                if (map[i][j] == '0' && neighbours == 3){
+                    newMap[i][j] = '1';
+                } else if (map[i][j] == '1' && (neighbours == 2 || neighbours == 3)){
+                    newMap [i][j] = '1';
+                }
+                else newMap [i][j] = '0';
+            }
+        }
+    return newMap;
+    }
+
+
+    public void showMap() {
+        for (int i = 0; i <map.length; i++) {
+            for (int j = 0; j<map[0].length; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+    }
+
+    public void setMap(char[][] map) {
+        this.map = map;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,17 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String filename = "example.txt";
+        String filename = "torus.txt";
         MapCreator mp = new MapCreator(filename);
-        mp.showMap();
         int simLength = mp.getLength();
+        CellManager cm = new CellManager(mp.getMap(), simLength);
+        cm.showMap();
+        System.out.println(cm.CountNeighbours(mp.getMap(), 0, 0));
+
+        for(int i = 0; i <= 5; i++) {
+            char[][] newMap = cm.newIteration(mp.getMap());
+            cm.setMap(newMap);
+            cm.showMap();
+        }
     }
 }
